f9535de6e55bfe2bb92cb9fcd23a1ce53b40577e
IsmaelPerezRodriguez (UNI) <ismaeluniperez@gmail.com>
2025-03-25 20:18:09 +0100

    -Animacion de corazon -Animacion de moneda

--- Assets/Scripts/Colisiones/ColisionLlave.cs
+++ Assets/Scripts/Colisiones/ColisionLlave.cs
@@ -2,13 +2,17 @@ using UnityEngine;
 
 public class ColisionLlave : MonoBehaviour
 {
+    private bool recogida = false;
     void OnTriggerEnter(Collider other)
     {
+        if (recogida) return; // Si ya se recogió, no hacer nada
+
         if (other.CompareTag("Jugador"))
         {
             InventarioJugador inventario = other.GetComponent<InventarioJugador>();
             if (inventario != null)
             {
+                recogida = true; // Marca la llave como recogida
                 inventario.AgregarLlaves();
                 Debug.Log("Llave recogida");
                 Destroy(gameObject);
